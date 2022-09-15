607b392f8afd7fe9ca6022dfbbd9e41cd8840838
oeinfantep1102 <omar.infante.mt@usa.edu.co>
2022-09-14 21:53:36 -0500

    Proyecto Cinema Esquema Inicial

---
+++ src/main/java/com/grupo18/cinema/CinemaApplication.java
@@ -0,0 +1,14 @@
+package com.grupo18.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+                System.out.println("Hola Proyecto");
+	}
+
+}
